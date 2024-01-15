70140c5ab343348f6dec325ef1aff95ab79895a1
長谷川 達人 <t-hase@rc.u-fukui.ac.jp>
2024-01-15 15:45:15 +0900

    階乗の計算処理を追加

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Hello World.");
+		int num = Integer.parseInt(args[0]);
+		int res = 1;
+		for(int i=1;i<=num;i++){
+			res *= i;
+		}
+		System.out.println("階乗=" + res);
 	}
 }
